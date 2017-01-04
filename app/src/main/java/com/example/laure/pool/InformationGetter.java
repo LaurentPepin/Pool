3b86e6efbfa2390e593a31b6643a3e388c474a0a
LaurentPepin <laurent.pepin@polymtl.ca>
2017-01-03 19:10:34 -0500

    Table is finished. Stats from yesterday and total stats are displayed.

--- app/src/main/java/com/example/laure/pool/InformationGetter.java
+++ app/src/main/java/com/example/laure/pool/InformationGetter.java
@@ -1,8 +1,11 @@
 package com.example.laure.pool;
 
 import android.content.Context;
+import android.icu.text.DecimalFormat;
+import android.icu.text.NumberFormat;
 import android.os.AsyncTask;
 import android.support.v4.content.ContextCompat;
+import android.util.Log;
 import android.view.Gravity;
 import android.view.ViewGroup;
 import android.widget.ScrollView;
@@ -27,8 +30,7 @@ public class InformationGetter extends AsyncTask<Void,Void,Void> {
     TextView textView;
     String[] poolersNames;
     int nPoolers;
-    int[] yesterdayStats;
-    int[] totalStats;
+    int[] stats;
 
     public InformationGetter(Context ctx,TableLayout tb, TextView tx){
         context = ctx;
@@ -42,46 +44,21 @@ public class InformationGetter extends AsyncTask<Void,Void,Void> {
             Document document = Jsoup.connect("https://www.marqueur.com/poolpepin2015").get();
 
             String title = document.select(".titre").first().text();
-
             if(title.equals("EN DIRECT")){
                 isLive = true;
             }
 
+            Element mainTable;
+
             if(isLive){
-                //find number of poolers and get poolers names
-                Element live = document.select(".titre").first().parent();
-                Element tableLive = document.select(live.cssSelector() + " .t10gc").first().parent().parent();
-                String text = document.select(tableLive.cssSelector() + " .t12b_n").text();
-                String[] separatedNames = text.split(" ");
-                nPoolers = separatedNames.length / 2;
-
-                Element mainTable = document.select(".titre").get(1).parent().parent().parent().parent();
-
-                //Get poolers names
-                text = document.select(mainTable.cssSelector() + " .t12b_n").text();
-                separatedNames = text.split(" ");
-                poolersNames = getFullNames(separatedNames);
-
-
-                //Get stats
-                String stats = document.select(mainTable.cssSelector() + " .t12nc").text();
-                String[] separatedStats = stats.split(" ");
-                getStats(separatedStats);
-
+                findNPoolersLive(document);
+                mainTable = document.select(".titre").get(1).parent().parent().parent().parent();
             }
             else {
-                Element mainTable = document.select(".t12b_n").first().parent().parent().parent();
-
-                //Get poolers names
-                String text = document.select(mainTable.cssSelector() + " .t12b_n").text();
-                String[] separatedNames = text.split(" ");
-                poolersNames = getFullNames(separatedNames);
-
-                //Get stats
-                String stats = document.select(mainTable.cssSelector() + " .t12nc").text();
-                String[] separatedStats = stats.split(" ");
-                getStats(separatedStats);
+                mainTable = document.select(".t12b_n").first().parent().parent().parent();
             }
+
+            getInformation(document,mainTable);
 
         }
         catch (Exception e){e.printStackTrace();}
@@ -89,49 +66,56 @@ public class InformationGetter extends AsyncTask<Void,Void,Void> {
         return null;
     }
 
-    public void getStats(String[] strings){
-        String[] wantedStats = new String[nPoolers*2];
-        for(int i=0; i<nPoolers*2; i++){
-            wantedStats[i] = strings[i*2+1];
-        }
-        int[] yStats = new int[nPoolers];
-        for(int i=0; i<nPoolers; i++){
-            if(!wantedStats[i*2].equals("-")) {
-                yStats[i] = Integer.parseInt(wantedStats[i * 2]);
-            }
-            else{
-                yStats[i] = 0;
-            }
-        }
-        int[] tStats = new int[nPoolers];
-        for(int i=0; i<nPoolers; i++){
-            if(!wantedStats[i*2+1].equals("-")) {
-                tStats[i] = Integer.parseInt(wantedStats[i * 2 + 1]);
-            }
-            else{
-                tStats[i] = 0;
-            }
-        }
-        yesterdayStats = yStats;
-        totalStats = tStats;
+    public void findNPoolersLive (Document document){
+        Element live = document.select(".titre").first().parent();
+        Element tableLive = document.select(live.cssSelector() + " .t10gc").first().parent().parent();
+        String text = document.select(tableLive.cssSelector() + " .t12b_n").text();
+        String[] separatedNames = text.split(" ");
+        nPoolers = separatedNames.length / 2;
     }
 
-    public String[] getFullNames (String[] strings){
+    public void getInformation(Document document, Element element){
+        getFullNames(document,element);
+        getStats(document,element);
+    }
+
+    public void getFullNames (Document document, Element element){
+
+        String stringPoolersNames = document.select(element.cssSelector() + " .t12b_n").text();
+        String[] separatedNames = stringPoolersNames.split(" ");
+
         if(!isLive) {
-            nPoolers = strings.length / 2;
+            nPoolers = separatedNames.length / 2;
         }
-        String[] names = new String[nPoolers];
+        poolersNames = new String[nPoolers];
         for(int i=0; i<nPoolers; i++){
             //names[i] = strings[i*2] + " " + strings[i*2+1].substring(0,1) + ".";
-            names[i] = strings[i*2];
-            int index = names[i].indexOf('-');
+            poolersNames[i] = separatedNames[i*2];
+            int index = poolersNames[i].indexOf('-');
             if(index>0){
-                names[i] = names[i].substring(0,index+2) + ".";
+                poolersNames[i] = poolersNames[i].substring(0,index+2) + ".";
             }
-
         }
-        return names;
     }
+
+    public void getStats(Document document, Element element){
+
+        String statistics = document.select(element.cssSelector() + " .t12nc").text();
+        String[] separatedStats = statistics.split(" ");
+
+        stats = new int[nPoolers*4];
+
+        for(int i=0; i<nPoolers*4; i++){
+            if(!separatedStats[i].equals("-")) {
+                stats[i] = Integer.parseInt(separatedStats[i]);
+            }
+            else {
+                stats[i] = 0;
+            }
+        }
+    }
+
+
 
 
     @Override
@@ -142,72 +126,18 @@ public class InformationGetter extends AsyncTask<Void,Void,Void> {
             textView.setText(R.string.Direct);
         }
 
-        TableRow.LayoutParams params = new TableRow.LayoutParams(TableRow.LayoutParams.MATCH_PARENT, TableRow.LayoutParams.MATCH_PARENT, 1.0f);
-
-
         TableLayout tableLayoutNew = new TableLayout(context);
-        tableLayoutNew.setPadding(0,0,0,0);
+
         for(int i=0; i<nPoolers; i++){
 
             TableRow tableRow = new TableRow(context);
             tableRow.setGravity(Gravity.CENTER);
-            tableRow.setLayoutParams(params);
-            if(i%2==1){
+            tableRow.setLayoutParams(new TableRow.LayoutParams(TableRow.LayoutParams.MATCH_PARENT, TableRow.LayoutParams.MATCH_PARENT, 1.0f));
 
-                if(i==nPoolers-1){
-                    tableRow.setBackgroundResource(R.drawable.lastrowodd);
-                }
-                else {
-                    tableRow.setBackgroundResource(R.drawable.rowodd);
-                }
-            }
-            else{
-                if(i==nPoolers-1){
-                    tableRow.setBackgroundResource(R.drawable.lastroweven);
-                }
-                else {
-                    tableRow.setBackgroundResource(R.drawable.roweven);
-                }
-            }
+            setTableRowBackground(i,tableRow);
 
+            fillTableRow(i,tableRow);
 
-
-            for(int j=0; j<7; j++){
-                TextView textView = new TextView(context);
-
-                String text;
-                if(j==0) {
-                    text = " " + Integer.toString(i + 1) + " " + poolersNames[i];
-                }
-                else if (j==1){
-                    text = "" + yesterdayStats[i];
-                    textView.setGravity(Gravity.CENTER);
-                }
-                else {
-                    text = ""+totalStats[i];
-                    textView.setGravity(Gravity.CENTER);
-                }
-                textView.setText(text);
-                textView.setPadding(2,2,2,2);
-                textView.setTextColor(ContextCompat.getColor(context,R.color.black));
-                TableRow.LayoutParams layoutParams;
-
-                if(j==0){
-                    layoutParams = new TableRow.LayoutParams(0, ViewGroup.LayoutParams.WRAP_CONTENT,0.3f);
-                }
-                else {
-                    layoutParams = new TableRow.LayoutParams(0, ViewGroup.LayoutParams.WRAP_CONTENT,0.1f);
-                }
-                textView.setLayoutParams(layoutParams);
-                if(j==1 || j==4){
-                    textView.setBackgroundResource(R.drawable.borderleft2);
-                }
-                else if (j==2 || j==5){
-                    textView.setBackgroundResource(R.drawable.bordermiddle1);
-                }
-
-                tableRow.addView(textView);
-            }
             tableLayoutNew.addView(tableRow);
         }
         ScrollView scrollView = new ScrollView(context);
@@ -215,4 +145,80 @@ public class InformationGetter extends AsyncTask<Void,Void,Void> {
         tableLayout.addView(scrollView);
         tableLayout.setBackgroundResource(R.drawable.border);
     }
+
+    public void setTableRowBackground(int i, TableRow tableRow){
+        if(i%2==1){
+            if(i==nPoolers-1){
+                tableRow.setBackgroundResource(R.drawable.lastrowodd);
+            }
+            else {
+                tableRow.setBackgroundResource(R.drawable.rowodd);
+            }
+        }
+        else{
+            if(i==nPoolers-1){
+                tableRow.setBackgroundResource(R.drawable.lastroweven);
+            }
+            else {
+                tableRow.setBackgroundResource(R.drawable.roweven);
+            }
+        }
+    }
+
+    public void fillTableRow(int i,TableRow tableRow){
+
+        for(int j=0; j<7; j++){
+            TextView textView = new TextView(context);
+            textView.setGravity(Gravity.CENTER);
+            String text = "";
+
+            if(j==0) {
+                text = " " + Integer.toString(i + 1) + " " + poolersNames[i];
+                textView.setGravity(Gravity.NO_GRAVITY);
+            }
+            else if (j==1){
+                text = "" + stats[i*4];
+            }
+            else if (j==2){
+                text = "" + stats[i*4+1];
+            }
+            else if (j==3){
+                double moy = stats[i*4+1]*1.0f/stats[i*4];
+                text = String.format("%.2f", moy);
+
+            }
+            else if (j==4){
+                text = "" + stats[i*4+2];
+            }
+            else if (j==5){
+                text = "" + stats[i*4+3];
+            }
+            else {
+                double moy = stats[i*4+3]*1.0f/stats[i*4+2];
+                text = String.format("%.2f", moy);
+            }
+            textView.setText(text);
+            textView.setPadding(2,2,2,2);
+            textView.setTextColor(ContextCompat.getColor(context,R.color.black));
+            TableRow.LayoutParams layoutParams;
+
+            if(j==0){
+                layoutParams = new TableRow.LayoutParams(0, ViewGroup.LayoutParams.WRAP_CONTENT,0.3f);
+            }
+            else {
+                layoutParams = new TableRow.LayoutParams(0, ViewGroup.LayoutParams.WRAP_CONTENT,0.1f);
+            }
+            textView.setLayoutParams(layoutParams);
+            if(j==1 || j==4){
+                textView.setBackgroundResource(R.drawable.borderleft2);
+            }
+            else if (j==2 || j==5){
+                textView.setBackgroundResource(R.drawable.bordermiddle1);
+            }
+
+            tableRow.addView(textView);
+        }
+    }
+
 }
+
